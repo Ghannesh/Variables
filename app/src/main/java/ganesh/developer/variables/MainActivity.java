f83884640d58902d8cc5a21915dea3a35716dcf4
Ramakrishna Sayee <mramakrishnasayee@gmail.com>
2019-07-21 00:59:29 +0530

    Initislized git repository

---
+++ app/src/main/java/ganesh/developer/variables/MainActivity.java
@@ -0,0 +1,35 @@
+package ganesh.developer.variables;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        int age = 28;
+//        float price = 19.9f; //float, double
+//        double userscore = 5.3333333;
+//        long CoumputerRam = 10243472347l;
+//
+//        boolean isalive = true;
+//
+//        String name = "Ganesh";
+//        char letterG = 'G';
+//
+//        Log.i( "MyTag", CoumputerRam + "");
+
+        TextView text = findViewById(R.id. Text);
+
+        int sumTwoIntegerNumbers = 12 + 15;
+        text.setText(sumTwoIntegerNumbers + "");
+
+
+    }
+}
